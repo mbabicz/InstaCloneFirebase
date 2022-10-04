2dcacbef0cbd2b4a5ca29453d7ae3e48fa51b339
kz <michal@babicz.dev>
2022-10-04 21:05:42 +0200

    displaying firebase data in cells

--- InstaCloneFirebase/FeedViewController.swift
+++ InstaCloneFirebase/FeedViewController.swift
@@ -15,6 +15,11 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBOutlet weak var nameText: UILabel!
     
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         nameText.text = Auth.auth().currentUser?.email
@@ -25,15 +30,55 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Do any additional setup after loading the view.
     }
     
+    func getDataFromFirestore(){
+        
+        let fireStoreDatabase = Firestore.firestore()
+        
+        fireStoreDatabase.collection("Posts").addSnapshotListener { (snapshot, error) in
+            if error != nil{
+                print(error?.localizedDescription)
+                
+            }
+            else{
+                if(snapshot?.isEmpty != true && snapshot != nil){
+                    for document in snapshot!.documents{
+                        let documentID = document.documentID
+                        print(documentID)
+                        //getting value of postedBy in firebase structure
+                        if let postedBy = document.get("postedBy") as? String{
+                            self.userEmailArray.append(postedBy)
+                        }
+                        if let postComment = document.get("postComment") as? String{
+                            self.userCommentArray.append(postComment)
+                        }
+                        if let likes = document.get("likes") as? Int{
+                            self.likeArray.append(likes)
+                        }
+                        if let imageUrl = document.get("imageUrl") as? String{
+                            self.userImageArray.append(imageUrl)
+                        }
+                        
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+            }
+            
+            
+            
+            
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.likeLabel.text = "0"
-        cell.commentLabel.text = " test"
-        cell.usernameLabel.text = "test@test.test"
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.usernameLabel.text = userEmailArray[indexPath.row]
         cell.userImageView.image = UIImage(named: "select.png")
         return cell
     }
